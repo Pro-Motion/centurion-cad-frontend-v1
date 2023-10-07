42adea0a786d2536760534bd22354a4c683ce07d
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-10-07 21:20:42 +0200

    add styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,32 @@
 module.exports = {
   content: [],
   theme: {
-    screens: {},
+    screens: {},  
+      
+    backgroundColor: {
+      'dark-gradient-background': 'linear-gradient(284deg, #0E181E 0%, #222C32 122.25%))',
+      'block-background': '#354249',
+      'block-headline-backgroud': '#182228',
+      'card-background': '#232E34',
+      'border-grey': '#7F919B',
+      'border-red': '#A74141'
+    },
     colors: {
-      'dark-gradient-background': ''
+      'base-white-main': '#FFFFFF',
+      'base-brown-accent': '#A6876D',
+      'base-grey-sub': '#7F919B',
+      'base-black-accent': '#182228',
+    // STATUSES
+      'status-green': '#4AA741',
+      'status-yellow': '#DCCD43',
+      'status-red': '#A74141'
+    },
+    // ICONS
+    fill: {
+      'icon-white-main': '#FFFFFF',
+      'icon-grey-hover': '7F919B',
+      'icon-olive-active': '#2C747E',
+      'icon-disabled': '#BDC3C7'
     },
     fontFamily: {},
 
