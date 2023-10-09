6df8301b9e95d6d746c96974d000f1b2a1a60970
unknown <perebziak.vladislav@gmail.com>
2023-10-09 20:52:13 +0300

    add linter setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,23 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    node: true
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ]
+  },
   parserOptions: {
     ecmaVersion: 'latest'
   }
