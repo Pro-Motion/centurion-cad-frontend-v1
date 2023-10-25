99d6f44187f4735b7af2b8112c07529d6a0a8226
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-10-26 00:20:55 +0200

    add some styles

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,121 +3,131 @@
 // MAIN COLORS
 const { WHITE, BROWN, GREEN, YELLOW, RED, GREY, BLACK, GREY_BLUE, DARK_GREY, OLIVE } =
   Object.freeze({
-    WHITE: "#FFFFFF",
-    BROWN: "#A6876D",
-    GREEN: "#4AA741",
-    YELLOW: "#DCCD43",
-    RED: "#A74141",
-    GREY: "#7F919B",
-    BLACK: "#182228",
-    GREY_BLUE: "#232E34",
-    DARK_GREY: "#354249",
-    OLIVE: "#2C747E",
-  });
+    WHITE: '#FFFFFF',
+    BROWN: '#A6876D',
+    GREEN: '#4AA741',
+    YELLOW: '#DCCD43',
+    RED: '#A74141',
+    GREY: '#7F919B',
+    BLACK: '#182228',
+    GREY_BLUE: '#232E34',
+    DARK_GREY: '#354249',
+    OLIVE: '#2C747E'
+  })
 
 const THEME = {
   COLORS: {
     GENERAL: {
       DARK: {
         FILL: {
-          BACKGROUND_LINEAR: "linear-gradient(284deg, #0E181E 0%, #222C32 122.25%))",
+          BACKGROUND_LINEAR: 'linear-gradient(284deg, #0E181E 0%, #222C32 122.25%))',
           BACKGROUND_BLOCK: DARK_GREY,
           BACKGROUND_BLOCK_HEADLINE: BLACK,
-          BACKGROUND_CARD: GREY_BLUE,
+          BACKGROUND_CARD: GREY_BLUE
         },
         STROKE: {
           GREY_BORDER: GREY,
-          RED_BORDER: RED,
-        },
-      },
+          RED_BORDER: RED
+        }
+      }
     },
     TEXT: {
       BASE: {
         WHITE_MAIN: WHITE,
         BROWN_ACCENT: BROWN,
         GREY_SUB: GREY,
-        BLACK_ACCENT: BLACK,
+        BLACK_ACCENT: BLACK
       },
       STATUS: {
         GREEN,
         YELLOW,
-        RED,
-      },
+        RED
+      }
     },
     ICONS: {
       FILL: {
         DEAFULT: WHITE,
         HOVER: GREY,
         ACTIVE: OLIVE,
-        DISABLED: "#BDC3C7",
-      },
+        DISABLED: '#BDC3C7'
+      }
     },
     // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
     BUTTONS: {
       // VARIANT
       MAIN_BLUE: {
-        DEFAULT: { BACKGROUND: OLIVE, TEXT: WHITE, BORDER: "" },
+        DEFAULT: { BACKGROUND: OLIVE, TEXT: WHITE, BORDER: '' },
         HOVER: {
           TEXT: WHITE,
-          BACKGROUND: "#18606A",
-          BORDER: "",
+          BACKGROUND: '#18606A',
+          BORDER: ''
         },
         ACTIVE: {
           BACKGROUND: BLACK,
           TEXT: WHITE,
-          BORDER: "",
+          BORDER: ''
         },
         DISABLED: {
-          BACKGROUND: "#A5B9BC",
+          BACKGROUND: '#A5B9BC',
           TEXT: WHITE,
-          BORDER: "",
-        },
+          BORDER: ''
+        }
       },
 
       // VARIANT
       NAVIGATION_DARK: {
-        DEFAULT: { BACKGROUND: BLACK, TEXT: WHITE, BORDER: "" },
-        HOVER: { BACKGROUND: "#091217", TEXT: WHITE, BORDER: "" },
-        ACTIVE: { BACKGROUND: OLIVE, TEXT: WHITE, BORDER: "" },
-        DISABLED: { BACKGROUND: "#27353E", TEXT: WHITE, BORDER: "" },
+        DEFAULT: { BACKGROUND: BLACK, TEXT: WHITE, BORDER: '' },
+        HOVER: { BACKGROUND: '#091217', TEXT: WHITE, BORDER: '' },
+        ACTIVE: { BACKGROUND: OLIVE, TEXT: WHITE, BORDER: '' },
+        DISABLED: { BACKGROUND: '#27353E', TEXT: WHITE, BORDER: '' }
       },
 
       // VARIANT
       RED: {
-        DEFAULT: { BACKGROUND: RED, TEXT: WHITE, BORDER: "" },
+        DEFAULT: { BACKGROUND: RED, TEXT: WHITE, BORDER: '' },
         HOVER: {
           TEXT: WHITE,
-          BACKGROUND: "#932D2D",
-          BORDER: "",
+          BACKGROUND: '#932D2D',
+          BORDER: ''
         },
-        ACTIVE: { TEXT: "", BACKGROUND: BLACK, BORDER: "" },
-        DISABLED: { TEXT: "", BACKGROUND: "#D1A9A6", BORDER: "" },
+        ACTIVE: { TEXT: '', BACKGROUND: BLACK, BORDER: '' },
+        DISABLED: { TEXT: '', BACKGROUND: '#D1A9A6', BORDER: '' }
       },
 
       // VARIANT
       GREY: {
-        DEFAULT: { BACKGROUND: "rgba(127, 145, 155, 0.60)", BORDER: "" },
+        DEFAULT: { BACKGROUND: 'rgba(127, 145, 155, 0.60)', BORDER: '' },
         HOVER: {
-          TEXT: "",
-          BACKGROUND: "rgba(97, 115, 125, 0.60)",
-          BORDER: "",
+          TEXT: '',
+          BACKGROUND: 'rgba(97, 115, 125, 0.60)',
+          BORDER: ''
         },
-        ACTIVE: { TEXT: "", BACKGROUND: "rgba(24, 34, 40, 0.60)", BORDER: "" },
-        DISABLED: { TEXT: "", BACKGROUND: "#BDC3C7", BORDER: "" },
-      },
+        ACTIVE: { TEXT: '', BACKGROUND: 'rgba(24, 34, 40, 0.60)', BORDER: '' },
+        DISABLED: { TEXT: '', BACKGROUND: '#BDC3C7', BORDER: '' }
+      }
     },
 
     INPUT: {
-      BACKGROUND: { DEFAULT: "#2B383F", DISABLED: DARK_GREY, ERROR: "#2B383F" },
+      BACKGROUND: {
+        DEFAULT: '#2B383F',
+        HOVER: '',
+        DISABLED: DARK_GREY,
+        ERROR: '#2B383F'
+      },
       BORDER: {
         DEFAULT: BLACK,
         HOVER: DARK_GREY,
         ACTIVE: OLIVE,
         DISABLED: OLIVE,
-        ERROR: "#733B3B",
+        ERROR: '#733B3B'
       },
-      PLACEHOLDER: { DEFAULT: GREY, DISABLED: "#6B7D87" },
-    },
+      PLACEHOLDER: {
+        DEFAULT: GREY,
+        DISABLED: '#6B7D87',
+        HOVER: '',
+        ERROR: ''
+      }
+    }
   },
 
   FONTS: {
@@ -128,65 +138,65 @@ const THEME = {
         HEADLINES: {
           HEADLINE_ONE: {},
           HEADLINE_TWO: {},
-          HEADLINE_THREE: {},
+          HEADLINE_THREE: {}
         },
         TEXT: {
           TITLE: {
-            "line-height": 1.25,
-            "font-size": "16px",
-            "font-weight": 500,
+            'line-height': 1.25,
+            'font-size': '16px',
+            'font-weight': 500
           },
           REGULAR: {
-            "line-height": 1.25,
-            "font-size": "16px",
-            "font-weight": 400,
+            'line-height': 1.25,
+            'font-size': '16px',
+            'font-weight': 400
           },
           STATUS: {
-            "line-height": 1.25,
-            "font-size": "16px",
-            "font-weight": 700,
-            "letter-spacing": " 0.8px",
-            "text-transform": "uppercase",
-          },
+            'line-height': 1.25,
+            'font-size': '16px',
+            'font-weight': 700,
+            'letter-spacing': ' 0.8px',
+            'text-transform': 'uppercase'
+          }
         },
         BUTTONS: {
           LABEL: [
-            "font-size: 20px;",
-            "font-style: normal;",
-            "font-weight: 500;",
-            "line-height: 1.2;",
-            "text-transform: uppercase;",
+            'font-size: 20px;',
+            'font-style: normal;',
+            'font-weight: 500;',
+            'line-height: 1.2;',
+            'text-transform: uppercase;'
           ],
           NAVIGATION: [
-            "font-size: 20px;",
-            "font-style: normal;",
-            "font-weight: 500;",
-            "line-height: 1.2;",
-          ],
+            'font-size: 20px;',
+            'font-style: normal;',
+            'font-weight: 500;',
+            'line-height: 1.2;'
+          ]
         },
         FIELDS: {
           INPUT_TEXT: [
-            "font-size: 16px;",
-            "font-style: normal;",
-            "font-weight: 400;",
-            "line-height: 1.25;",
+            'font-size: 16px;',
+            'font-style: normal;',
+            'font-weight: 400;',
+            'line-height: 1.25;'
           ],
           PLACEHOLDER: [
-            ["font-size", "12px;"],
-            ["font-style", "normal;"],
-            ["font-weight", "400;"],
-            ["line-height", "1.33;"],
-          ],
-        },
-      },
+            ['font-size', '12px;'],
+            ['font-style', 'normal;'],
+            ['font-weight', '400;'],
+            ['line-height', '1.33;']
+          ]
+        }
+      }
     },
-    ALEGREYA: { FAMILY: "'Alegreya Sans SC', sans-serif" },
+    ALEGREYA: { FAMILY: "'Alegreya Sans SC', sans-serif" }
   },
 
   ANIMATIONS: {
-    HOVER_EFFECT: "300ms cubic-bezier(.89,.91,0,.65)",
-    LAMP_EFFECT: "",
-  },
-};
+    HOVER_EFFECT: '300ms cubic-bezier(.89,.91,0,.65)',
+    LAMP_EFFECT: ''
+  }
+}
 
-export default THEME;
+export default THEME
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,10 @@ module.exports = {
     // './src/components/**/*.{js,vue,ts}',
   ],
   theme: {
-    screens: {},
-
+    animations: {
+      'effect-hover': '300ms cubic-bezier(.89,.91,0,.65)',
+      'lamp-effect-hover': ''
+    },
     backgroundColor: {
       'dark-gradient-background': 'linear-gradient(284deg, #0E181E 0%, #222C32 122.25%))',
       'block-background': '#354249',
@@ -25,7 +27,93 @@ module.exports = {
       // STATUSES
       'status-green': '#4AA741',
       'status-yellow': '#DCCD43',
-      'status-red': '#A74141'
+      'status-red': '#A74141',
+      // TEXTS
+      'text-base-white-main': '#FFFFFF',
+      'text-base-brown-accent': '#A6876D',
+      'text-base-grey-sub': '#7F919B',
+      'text-base-black-accent': '#182228',
+      'text-status-green': '#4AA741',
+      'text-status-yellow': '#DCCD43',
+      'text-status-red': '#A74141',
+      // GENERAL
+      'dark-fill-bg_linear': 'linear-gradient(284deg, #0E181E 0%, #222C32 122.25%))',
+      'dark-fill-bg_block': '#354249',
+      'dark-fill-bg_block_headline': '#182228',
+      'dark-fill-bg_card': '#232E34',
+      // STOKE
+      'dark-stoke-border_grey': '#7F919B',
+      'dark-stoke-border_red': '#A74141',
+      // BUTTONS - variant main-blue
+      'btn-main-blue': {
+        'btn-text': '#FFFFFF',
+        // default
+        'default-bg': '#2C747E',
+        'default-border': '',
+        // hover
+        'hover-bg': '#18606A',
+        'hover-border': '',
+        // active
+        'active-bg': '#182228',
+        'active-border': '',
+        // disabled
+        'disabled-bg': '#A5B9BC',
+        'disabled-border': ''
+      },
+      // BUTTONS - variant navigation dark
+      'btn-nav-dark': {
+        'btn-text': '#FFFFFF',
+        // default
+        'default-bg': '#182228',
+        'default-border': '',
+        // hover
+        'hover-bg': '#091217',
+        'hover-border': '',
+        // active
+        'active-bg': '#2C747E',
+        'active-border': '',
+        // disabled
+        'disabled-bg': '#27353E',
+        'disabled-border': ''
+      },
+      // BUTTONS - variant red
+      'btn-main-red': {
+        // default
+        'default-bg': '',
+        'default-text': '#FFFFFF',
+        'default-border': '',
+        // hover
+        'hover-bg': '#932D2D',
+        'hover-text': '#FFFFFF',
+        'hover-border': '',
+        // active
+        'active-bg': '#182228',
+        'active-text': '',
+        'active-border': '',
+        // disabled
+        'disabled-bg': '#D1A9A6',
+        'disabled-text': '',
+        'disabled-border': ''
+      },
+      // BUTTONS - variant grey
+      'btn-grey': {
+        // default
+        'default-bg': 'rgba(127, 145, 155, 0.60)',
+        'default-text': '',
+        'default-border': '',
+        // hover
+        'hover-bg': 'rgba(97, 115, 125, 0.60)',
+        'hover-text': '',
+        'hover-border': '',
+        // active
+        'active-bg': 'rgba(24, 34, 40, 0.60)',
+        'active-text': '',
+        'active-border': '',
+        // disabled
+        'disabled-bg': '#BDC3C7',
+        'disabled-text': '',
+        'disabled-border': ''
+      }
     },
     // ICONS
     fill: {
@@ -34,8 +122,32 @@ module.exports = {
       'icon-olive-active': '#2C747E',
       'icon-disabled': '#BDC3C7'
     },
-    fontFamily: {},
+    // INPUT
+    input: {
+      // default
+      'default-bg': '#2B383F',
+      'default-border': '#182228',
+      'default-placeholder': '#7F919B',
+      // hover
+      'hover-bg': '',
+      'hover-border': '#354249',
+      'hover-placeholder': '',
+      // active
+      'active-bg': '',
+      'active-border': '#2C747E',
+      'active-placeholder': '',
+      // disabled
+      'disabled-bg': '#354249',
+      'disabled-border': '#2C747E',
+      'disabled-placeholder': '#6B7D87',
+      // error
+      'error-bg': '#2B383F',
+      'error-border': '#733B3B',
+      'error-placeholder': ''
+    },
 
+    fontFamily: {},
+    screens: {},
     extend: {}
   },
   plugins: []
