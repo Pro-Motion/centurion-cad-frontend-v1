912566da5e602403face78bcc4c78fed0b1ecdf7
Olga Polishchuk <olgapolichuk@ukr.net>
2023-11-04 20:58:09 +0200

    add new icons 24px files

--- src/components/styled-library/StyledButton.vue
+++ src/components/styled-library/StyledButton.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import BaseSvg from '../base/BaseSvg.vue'
-import IconUser from '@/assets/lib/icons/20px/IconUser.vue'
+import IconAssign from '@/assets/lib/icons/24px/IconAssign.vue'
 defineProps({
   disabled: { type: Boolean, default: false },
   text: { type: String, default: 'Start' },
@@ -10,6 +10,6 @@ defineProps({
 <template>
   <button :type="type" class="button button-main border-olive" :disabled="disabled">
     {{ text }}
-    <BaseSvg iconName="cioc" width="20" height="20" iconColor="red"><IconUser /></BaseSvg>
+    <BaseSvg iconName="cioc" width="24" height="24" iconColor="red"><IconAssign /></BaseSvg>
   </button>
 </template>
