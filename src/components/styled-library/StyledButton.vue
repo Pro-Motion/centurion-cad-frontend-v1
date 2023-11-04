f369cb7508dcb445dc28a288e60f886b3b7e52d7
Olga Polishchuk <olgapolichuk@ukr.net>
2023-11-04 20:02:56 +0200

    add all 20px icons

--- src/components/styled-library/StyledButton.vue
+++ src/components/styled-library/StyledButton.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import BaseSvg from '../base/BaseSvg.vue'
-import IconMoreFunctions from '@/assets/lib/icons/20px/IconMoreFunctions.vue'
+import IconUser from '@/assets/lib/icons/20px/IconUser.vue'
 defineProps({
   disabled: { type: Boolean, default: false },
   text: { type: String, default: 'Start' },
@@ -10,6 +10,6 @@ defineProps({
 <template>
   <button :type="type" class="button button-main border-olive" :disabled="disabled">
     {{ text }}
-    <BaseSvg iconName="cioc" width="20" height="20" iconColor="red"><IconMoreFunctions /></BaseSvg>
+    <BaseSvg iconName="cioc" width="20" height="20" iconColor="red"><IconUser /></BaseSvg>
   </button>
 </template>
