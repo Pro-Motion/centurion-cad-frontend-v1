206f47643a098270054586bc5844139d2bc5f00a
Olga Polishchuk <olgapolichuk@ukr.net>
2023-11-05 22:38:26 +0200

    add 24px icons

--- src/assets/lib/icons/24px/IconExtraFeatures.vue
+++ src/assets/lib/icons/24px/IconExtraFeatures.vue
@@ -1,3 +1,7 @@
 <template>
-  <g> </g>
+  <g>
+    <circle cx="17.66" cy="12" r="2" />
+    <circle cx="12" cy="12" r="2" />
+    <circle cx="6.34" cy="12" r="2"
+  /></g>
 </template>
--- src/components/styled-library/StyledButton.vue
+++ src/components/styled-library/StyledButton.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import BaseSvg from '../base/BaseSvg.vue'
-import IconAssign from '@/assets/lib/icons/24px/IconAssign.vue'
+import IconEmergency from '@/assets/lib/icons/24px/IconEmergency.vue'
 defineProps({
   disabled: { type: Boolean, default: false },
   text: { type: String, default: 'Start' },
@@ -10,6 +10,6 @@ defineProps({
 <template>
   <button :type="type" class="button button-main border-olive" :disabled="disabled">
     {{ text }}
-    <BaseSvg iconName="cioc" width="24" height="24" iconColor="red"><IconAssign /></BaseSvg>
+    <BaseSvg iconName="cioc" width="24" height="24" iconColor="red"><IconEmergency /></BaseSvg>
   </button>
 </template>
