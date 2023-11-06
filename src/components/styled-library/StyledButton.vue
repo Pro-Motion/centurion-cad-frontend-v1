37a5cf16c3eea2556162e0c6c253bfee804032d7
Olga Polishchuk <olgapolichuk@ukr.net>
2023-11-06 11:16:07 +0200

    add 32px icons

--- src/components/styled-library/StyledButton.vue
+++ src/components/styled-library/StyledButton.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import BaseSvg from '../base/BaseSvg.vue'
-import IconAddImage from '@/assets/lib/icons/32px/IconAddImage.vue'
+import IconWarrants from '@/assets/lib/icons/32px/IconWarrants.vue'
 defineProps({
   disabled: { type: Boolean, default: false },
   text: { type: String, default: 'Start' },
@@ -10,6 +10,6 @@ defineProps({
 <template>
   <button :type="type" class="button button-main border-olive" :disabled="disabled">
     {{ text }}
-    <BaseSvg iconName="cioc" width="32" height="32" iconColor="red"><IconAddImage /></BaseSvg>
+    <BaseSvg iconName="cioc" width="32" height="32" iconColor="red"><IconWarrants /></BaseSvg>
   </button>
 </template>
