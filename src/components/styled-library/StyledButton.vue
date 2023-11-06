82d23a4e69abb97a531e48b896e8f76e348bf879
taras <tarasgumqqq@gmail.com>
2023-11-06 20:22:30 +0200

    added styled button component

--- src/components/styled-library/StyledButton.vue
+++ src/components/styled-library/StyledButton.vue
@@ -1,15 +1,48 @@
 <script setup lang="ts">
+import IconAddImage from '@/assets/lib/icons/32px/IconAddImage.vue'
 import BaseSvg from '../base/BaseSvg.vue'
-import IconUser from '@/assets/lib/icons/32px/IconUser.vue'
-defineProps({
+import { computed, useSlots } from 'vue'
+
+const props = defineProps({
   disabled: { type: Boolean, default: false },
   text: { type: String, default: 'Start' },
-  type: { type: String as () => 'button' | 'submit' | 'reset', default: 'button' }
+  type: { type: String as () => 'button' | 'submit' | 'reset', default: 'button' },
+  variant: {
+    type: String as () => 'main' | 'secondary' | 'close' | 'icon',
+    required: true,
+    default: 'main'
+  },
+  needIcon: { type: Boolean, default: false }
 })
+
+const showIcon = !!useSlots().default
+
+const emit = defineEmits(['click', 'submit', 'reset'])
+const clickOnButton = () => {
+  emit('click')
+}
+
+const onlyIcon = computed(() => {
+  return props.variant === 'close' || props.variant === 'icon'
+})
+//TODO: change w-full to native styles when last will ready
 </script>
+<!-- TODO: optimize onlyIcons with componentIS -->
 <template>
-  <button :type="type" class="button button-main border-olive" :disabled="disabled">
-    {{ text }}
-    <BaseSvg iconName="cioc" width="32" height="32" iconColor="red"><IconUser /></BaseSvg>
+  <button
+    :type="type"
+    :class="[`button-${variant}`, { 'w-full': variant != 'close' }]"
+    class="button border-olive"
+    :disabled="disabled"
+    @click="clickOnButton"
+  >
+    <BaseSvg v-if="variant == 'close'" iconName="close" width="20" height="20" iconColor="white">
+      <!--TODO: add closeIcon -->
+    </BaseSvg>
+    <!-- TODO: ask for sizes -->
+    <BaseSvg v-if="showIcon" iconName="addImage" width="32" height="32" iconColor="white">
+      <slot></slot>
+    </BaseSvg>
+    <div v-if="!onlyIcon">{{ text }}</div>
   </button>
 </template>
--- src/views/departments/CivilianView.vue
+++ src/views/departments/CivilianView.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 // import  from ""
+
+import IconAddImage from '@/assets/lib/icons/32px/IconAddImage.vue'
 import StyledBox from '@/components/styled-library/StyledBox.vue'
-import StyledInput from '@/components/styled-library/StyledInput.vue'
+import StyledButton from '@/components/styled-library/StyledButton.vue'
+
+function sub() {
+  alert('sub')
+}
 </script>
 
 <template>
   <div>
-    <StyledBox needHeader cardHeading="Emergency Call">
+    <IconAddImage />
+    <StyledBox needHeader cardHeading="Emergency Call ddddddddddd">
       <template #header></template>
+      <form @submit="sub">
+        <StyledButton variant="icon" type="submit" text="hi">
+          <IconAddImage />
+        </StyledButton>
+      </form>
     </StyledBox>
   </div>
 </template>
