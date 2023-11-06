6f006f9f01d643f71bb6d86d47844770d7881281
taras <tarasgumqqq@gmail.com>
2023-11-06 16:16:43 +0200

    added box component

--- src/views/departments/CivilianView.vue
+++ src/views/departments/CivilianView.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 // import  from ""
+import StyledBox from '@/components/styled-library/StyledBox.vue'
+import StyledInput from '@/components/styled-library/StyledInput.vue'
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <StyledBox needHeader cardHeading="Emergency Call">
+      <template #header></template>
+    </StyledBox>
+  </div>
 </template>
