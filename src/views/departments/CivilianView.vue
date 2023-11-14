7a446926b656d7dd6162e059263f7a53d47e5894
taras <tarasgumqqq@gmail.com>
2023-11-14 22:01:06 +0200

    clearing

--- src/views/departments/CivilianView.vue
+++ src/views/departments/CivilianView.vue
@@ -1,18 +1,11 @@
 <script setup lang="ts">
 // import  from ""
 
-import StyledBox from '@/components/styled-library/StyledBox.vue'
-
 function sub() {
   alert('sub')
 }
 </script>
 
 <template>
-  <div>
-    <StyledBox needHeader>
-      <template #header>dsad</template>
-      d
-    </StyledBox>
-  </div>
+  <div></div>
 </template>
