3942039c6b3503cbaa57b2c0e659b8ae13f17423
taras <tarasgumqqq@gmail.com>
2023-11-28 12:17:47 +0200

    added new styledMargin component

--- src/constants/index.js
+++ src/constants/index.js
@@ -6,5 +6,12 @@ const BASE_BUTTON_VARIANTS = Object.freeze({
   ICON: 'ICON',
   CLOSE: 'CLOSE'
 })
+const BASE_CONTENT_MARGINS = Object.freeze({
+  NONE: '0px',
+  SM: '8px',
+  MD: '12px',
+  L: '20px',
+  XL: '40px'
+})
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-export { BASE_NOTIFICATION_TYPES, BASE_BUTTON_VARIANTS }
+export { BASE_NOTIFICATION_TYPES, BASE_BUTTON_VARIANTS, BASE_CONTENT_RETREATS }
--- src/views/departments/CivilianView.vue
+++ src/views/departments/CivilianView.vue
@@ -3,6 +3,9 @@ import StyledBox from '@/components/styled-library/StyledBox.vue'
 import StyledTextBlock from '@/components/styled-library/StyledTextBlock.vue'
 import BaseSvg from '@/components/base/BaseSvg.vue'
 import IconEdit from '@/assets/lib/icons/24px/IconEdit.vue'
+import StyledRetreat from '../../components/styled-library/StyledRetreat.vue'
+import { BASE_CONTENT_MARGINS } from '../../constants'
+
 // function sub() {
 //   alert('sub')
 // }
@@ -10,27 +13,29 @@ import IconEdit from '@/assets/lib/icons/24px/IconEdit.vue'
 
 <template>
   <div>
-    <StyledBox needHeader cardHeading="Title">
-      <template #body>
-        <StyledTextBlock needHeader blockHeading="Some Title">
-          <template #header>
-            <BaseSvg width="24" height="24" :iconColor="white"> <IconEdit /> </BaseSvg>
-          </template>
-          <template #body
-            ><div>SOME FILLER HERE</div>
+    <StyledRetreat :retreatY="BASE_CONTENT_MARGINS.SM" :retreatX="BASE_CONTENT_MARGINS.XL">
+      <StyledBox needHeader cardHeading="Title">
+        <template #body>
+          <StyledTextBlock needHeader blockHeading="Some Title">
+            <template #header>
+              <BaseSvg width="24" height="24" iconColor="white"> <IconEdit /> </BaseSvg>
+            </template>
+            <template #body
+              ><div>SOME FILLER HERE</div>
 
-            <div>SOME FILLER HERE</div>
+              <div>SOME FILLER HERE</div>
 
-            <div>SOME FILLER HERE</div>
+              <div>SOME FILLER HERE</div>
 
-            <div>SOME FILLER HERE</div>
+              <div>SOME FILLER HERE</div>
 
-            <div>SOME FILLER HERE</div>
+              <div>SOME FILLER HERE</div>
 
-            <div>SOME FILLER HERE</div>
-          </template>
-        </StyledTextBlock>
-      </template></StyledBox
+              <div>SOME FILLER HERE</div>
+            </template>
+          </StyledTextBlock>
+        </template></StyledBox
+      ></StyledRetreat
     >
   </div>
 </template>
