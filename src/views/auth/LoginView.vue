08e760f3f665eeaf166b6829debbe256292da524
taras <tarasgumqqq@gmail.com>
2023-12-24 02:02:02 +0200

    auth bug fixed

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,18 +1,19 @@
 <script setup>
 import LoginForm from '@/components/auth/LoginForm.vue'
 import StyledBox from '@/components/styled-library/StyledBox.vue'
-// import { useHead } from '@vueuse/head'
+import { useHead } from '@unhead/vue'
 import { authApi } from '@/api/auth.api.js'
 import { useMutation, useQuery, useQueryClient } from 'vue-query'
 import { useAuthStore } from '@/store/auth.store.js'
 import { onBeforeUpdate } from 'vue'
-const authResult = useQuery('authUser', () => authApi.current(), {
-  enabled: false,
-  retry: 1
-})
-const queryClient = useQueryClient()
+
 const authStore = useAuthStore()
-const { isLoading, mutate } = useMutation((credentials) => authApi.login(credentials), {
+const {
+  isLoading,
+  mutate,
+  isSuccess,
+  data: authData
+} = useMutation((credentials) => authApi.login(credentials), {
   onError: (error) => {
     if (Array.isArray(error.response.data.error)) {
       error.response.data.error.forEach((el) => {
@@ -23,11 +24,10 @@ const { isLoading, mutate } = useMutation((credentials) => authApi.login(credent
     }
   },
   onSuccess: (data) => {
-    queryClient.refetchQueries('authUser')
     console.log('succesful')
-    const { name, accessToken } = JSON.stringify(data)
-    console.log('User:', name)
-    console.log('Token:', accessToken)
+    authStore.setAuthUser(JSON.stringify(data))
+    console.log(authStore.authUser)
+
     // router.push({ name: 'profile' })
   }
 })
@@ -38,22 +38,17 @@ function getData(values) {
     password: values.password
   })
 }
-onBeforeUpdate(() => {
-  if (authResult.isSuccess.value) {
-    const authUser = Object.assign({}, authResult.data.value?.data.user)
-    authStore.setAuthUser(authUser)
-  }
+
+useHead({
+  // Can be static or computed
+  title: 'Login | Centurion CAD',
+  meta: [
+    {
+      name: `description`,
+      content: ''
+    }
+  ]
 })
-// useHead({
-//   // Can be static or computed
-//   title: 'Login | Centurion CAD',
-//   meta: [
-//     {
-//       name: `description`,
-//       content: ''
-//     }
-//   ]
-// })
 </script>
 
 <template>
